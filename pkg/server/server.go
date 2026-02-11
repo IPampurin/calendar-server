bbf24f1a05361e7ffe9d640aae1d6a5d8a85172b
IPampurin <I.Pampurin@yandex.ru>
2026-02-11 11:17:48 +0500

    add Shutdown

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,9 +1,15 @@
 package server
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/IPampurin/calendar-server/pkg/api"
 	"github.com/IPampurin/calendar-server/pkg/storage"
@@ -32,11 +38,47 @@ func Run(db storage.Repository) error {
 	// оборачиваем в middleware
 	handler := LoggingMiddleware(logger)(http.DefaultServeMux)
 
+	// создаем и настраиваем сервер
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: handler,
+	}
+
+	// канал для shutdown
+	idleConnsClosed := make(chan struct{})
+
 	// http.Handle("/", http.FileServer(http.Dir("web"))) // здесь может быть запуск фронтэнда
 
-	// запускаем сервер
-	addr := fmt.Sprintf(":%s", port)
-	logger.Printf("Сервер запущен на %s\n", addr)
+	// горутина для graceful shutdown
+	go func() {
 
-	return http.ListenAndServe(addr, handler)
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, syscall.SIGINT, syscall.SIGTERM)
+		<-sigint
+
+		logger.Println("Получен сигнал остановки сервера.")
+
+		// останавливаем сервер (до окончания текущего соединения или 30 секунд)
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			log.Printf("Ошибка при остановке сервера: %v\n", err)
+		}
+
+		close(idleConnsClosed)
+	}()
+
+	// запускаем сервер
+	logger.Printf("Сервер запущен на %s\n", srv.Addr)
+
+	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("ошибка сервера: %w", err)
+	}
+
+	// ждём остановки сервера
+	<-idleConnsClosed
+	logger.Println("Сервер корректно остановлен.")
+
+	return nil
 }
